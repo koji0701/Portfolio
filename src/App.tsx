af5d27ea3a9c92076136107a3f2fd28f1b37f080
Koji Wong <kojirwong@gmail.com>
2025-02-02 21:53:17 -0600

    utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hero from './components/sections/Hero';
 import Projects from './components/sections/Projects';
 import Skills from './components/sections/Skills';
+import Work from './components/sections/Work';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white">
-      <Hero />
-      <Projects />
-      <Skills />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-black text-white">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Work />
+              <Projects />
+              <Skills />
+            </>
+          } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
