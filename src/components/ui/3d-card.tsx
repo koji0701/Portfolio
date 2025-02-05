e84e6b3a8db1b0d771c3b0ad3111ed7fdc8ccaaf
Koji Wong <kojirwong@gmail.com>
2025-02-05 15:47:46 -0600

    feat: downsized 3d cards, connected github links to projects

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -155,51 +155,51 @@ export const useMouseEnter = () => {
 };
 
 export function Card3D({title, description, image, link}: {
-    title: string;
-    description: string;
-    image?: string;
-    link?: string;
-  }) {
-    return (
-      <CardContainer className="inter-var">
-        <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+  title: string;
+  description: string;
+  image?: string;
+  link?: string;
+}) {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto rounded-xl p-4 border">
+        <CardItem
+          translateZ="50"
+          className="text-lg font-bold text-neutral-600 dark:text-white"
+        >
+          {title}
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-xs max-w-sm mt-2 dark:text-neutral-300"
+        >
+          {description}
+        </CardItem>
+        <CardItem
+          translateZ="100"
+          rotateX={20}
+          rotateZ={-10}
+          className="w-full mt-3"
+        >
+          <img
+            src={image}
+            className="h-36 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt={title}
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-12">
           <CardItem
-            translateZ="50"
-            className="text-xl font-bold text-neutral-600 dark:text-white"
+            translateZ={20}
+            translateX={-40}
+            as="a"
+            href={link}
+            className="px-3 py-1.5 rounded-xl text-xs font-normal dark:text-white hover:text-neutral-300 transition-colors"
           >
-            {title}
+            Learn More →
           </CardItem>
-          <CardItem
-            as="p"
-            translateZ="60"
-            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-          >
-            {description}
-          </CardItem>
-          <CardItem
-            translateZ="100"
-            rotateX={20}
-            rotateZ={-10}
-            className="w-full mt-4"
-          >
-            <img
-              src={image}
-              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-              alt={title}
-            />
-          </CardItem>
-          <div className="flex justify-between items-center mt-20">
-            <CardItem
-              translateZ={20}
-              translateX={-40}
-              as="a"
-              href={link}
-              className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white hover:text-neutral-300 transition-colors"
-            >
-              Learn More →
-            </CardItem>
-          </div>
-        </CardBody>
-      </CardContainer>
-    );
-  }
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
