78122981531b3caf3e55723651ff397e361a23d0
Koji Wong <kojirwong@gmail.com>
2025-06-09 12:31:47 -0700

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Hero from './components/sections/Hero';
 import Projects from './components/sections/Projects';
 import Skills from './components/sections/Skills';
@@ -45,6 +46,7 @@ function App() {
             } />
           </Routes>
         </div>
+        <Analytics />
       </div>
     </BrowserRouter>
   );
